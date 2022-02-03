76e1a4543c68f7f5cb2da092d9571bc95d505d8f
dema2006 <dementy.statsenko@qmail.com>
2022-02-03 17:37:58 +0500

    Задача 2(no work)

--- main(1).py
+++ main(1).py
@@ -4,29 +4,51 @@ import sys
 import pygame
 import requests
 
-map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
-response = requests.get(map_request)
 
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
+mpz = 15
+mpt = "map"
 
-# Запишем полученное изображение в файл.
 map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+
+
+def update_map():
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&z={mpz}&l={mpt}"
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    # Запишем полученное изображение в файл.
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
 
 # Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 # Рисуем картинку, загружаемую из только что созданного файла.
+update_map()
 screen.blit(pygame.image.load(map_file), (0, 0))
 # Переключаем экран и ждем закрытия окна.
-pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
-    pass
+    for i in pygame.event.get():
+        if i.type == pygame.KEYDOWN:
+            if i.key == 280 and mpz < 19:
+                mpz += 1
+                print(1)
+            if i.key == 281 and mpz > 2:
+                mpz -= 1
+                print(2)
+        if i.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+    update_map()
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+
 pygame.quit()
 
 # Удаляем за собой файл с изображением.
